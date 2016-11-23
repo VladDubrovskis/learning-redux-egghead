796d80095ea0ad39eb6b47dcd82d005bcda2d11b
Vlad Dubrovskis <vlad@vovka.co.uk>
2016-11-23 16:30:29 +0000

    generating containers with connect() from react-redux - VisibleTodoList

--- app.js
+++ app.js
@@ -178,34 +178,28 @@ const getVisibleTodos = (todos, filter) => {
   }
 }
 
-class VisibleTodoList extends Component {
-  componentDidMount() {
-    const { store } = this.context;
-    this.unsubscribe = store.subscribe(() =>
-      this.forceUpdate()
-    );
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    const props = this.props;
-    const { store } = this.context;
-    const state = store.getState();
-
-    return (
-      <TodoList
-      todos={getVisibleTodos(state.todos, state.visibilityFilter)}
-      onTodoClick={id => store.dispatch({
-        type: 'TOGGLE_TODO',
-        id
-      })}
-      />
-    )
+const mapStateToProps = (state) => {
+  return {
+    todos: getVisibleTodos(state.todos, state.visibilityFilter)
   }
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTodoClick: id => {
+      dispatch({
+        type: 'TOGGLE_TODO',
+        id
+      })
+    }
+  }
+}
+
+const { connect } = ReactRedux;
+const VisibleTodoList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList);
 
 VisibleTodoList.contextTypes = {
   store: React.PropTypes.object
